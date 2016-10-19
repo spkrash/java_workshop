5c08493f47f92faab8ebcf38131123331465040e
Sergii Pavlenko <spkrash@gmail.com>
2016-10-19 17:03:57 +0300

    Added packege for HelloWorld + some code refactoring

---
+++ ashjar/src/main/java/spkrash/krashinc/ashjar/HelloWorld.java
@@ -0,0 +1,10 @@
+package spkrash.krashinc.ashjar;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String target = "Alexei";
+		System.out.println("Hello, " + target + "!");
+	}
+}
