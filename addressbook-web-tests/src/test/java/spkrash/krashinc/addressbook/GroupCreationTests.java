90fe788cf0de45861181b0f716e57c678b233662
Sergii Pavlenko <spkrash@gmail.com>
2016-10-25 18:16:13 +0300

    Fixture refactoring, test method rename

--- addressbook-web-tests/src/test/java/spkrash/krashinc/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/spkrash/krashinc/addressbook/GroupCreationTests.java
@@ -3,28 +3,20 @@ package spkrash.krashinc.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
     FirefoxDriver wd;
     
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -33,6 +25,11 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation()
+    {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
@@ -49,15 +46,20 @@ public class GroupCreationTests {
     }
     
     @AfterMethod
-    public void tearDown() {
+    public void tearDown()
+    {
         wd.quit();
     }
     
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
+    public static boolean isAlertPresent(FirefoxDriver wd)
+    {
+        try
+        {
             wd.switchTo().alert();
             return true;
-        } catch (NoAlertPresentException e) {
+        }
+        catch (NoAlertPresentException e)
+        {
             return false;
         }
     }
